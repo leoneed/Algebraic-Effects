fb85c5b2c9af46f0748b5ba6e536669a91b76b17
Leonid Yeromin <toxicsoft@gmail.com>
2020-10-13 21:53:40 +0200

    Simple handler. New replaces old. Nothing set - error.

---
+++ simple.js
@@ -0,0 +1,25 @@
+const pause = async ms =>
+    new Promise(resolve =>
+        setTimeout(resolve, ms));
+
+const handle = (fn, handler) => fn.perform = handler;
+
+const getName = async (user) => {
+    if (!user.name) {
+        user.name = await getName.perform('ask_name');
+    }
+
+    return user.name;
+};
+
+handle(getName, async effect => {
+    if (effect === 'ask_name') {
+        await pause(1000);
+        return 'Arya Stark';
+    }
+});
+
+(async () => {
+    console.log(await getName({ name: 'Some Name' }));
+    console.log(await getName({ title: 'No Name' }));
+})();
